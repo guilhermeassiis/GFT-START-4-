3d0e9b1769de618965369c92c5e646b671deecc0
guilhermeassi1 <assis.guilhermef@gmail.com>
2022-03-15 18:16:42 -0300

    Atualizando

--- DesafioGFT/ImcDesafio4/Program.cs
+++ DesafioGFT/ImcDesafio4/Program.cs
@@ -5,8 +5,18 @@ public class Program
     // DESAFIO 4
     static void Main(string[] args)
     {
-        CalculadoraIMC calcImc = new("Guilherme", 80.0, 1.60);
+        Console.Write("Digite Seu nome: ");
+        string? nome = Console.ReadLine();
+        Console.WriteLine();
+        Console.Write("Digite seu peso: ");
+        double peso = double.Parse(Console.ReadLine());
+        Console.WriteLine();
+        Console.Write("Digitie sua altura: ");
+        double altura = double.Parse(Console.ReadLine());
+        
+        CalculadoraIMC calcImc = new(nome, peso, altura);
 
+        Console.WriteLine();
         Console.WriteLine(calcImc);
     }
 }
